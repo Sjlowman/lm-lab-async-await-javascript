70fe5a17dc36143cd9836c236000e725818b6efe
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-12 09:44:59 +0100

    Implement async solution

--- exercise001.js
+++ exercise001.js
@@ -1,3 +1,5 @@
+// Your first solution(s) to exercise001 here!
+/*
 const promise = new Promise((resolve, reject) => {
   // Generate random number between 0 and 9
   const randomInt = Math.floor(Math.random() * 10);
@@ -11,8 +13,6 @@ const promise = new Promise((resolve, reject) => {
   }
 });
 
-// Your solution(s) to exercise001 here!
-
 promise
   .then((value) => {
     console.log(`Yay! Promise resolved with response: ${value}`);
@@ -20,3 +20,29 @@ promise
   .catch((e) => {
     console.log(`Boo. Promise rejected with response: ${e}`);
   });
+  */
+
+function fiftyFityChance() {
+  return new Promise((resolve, reject) => {
+    const randomInt = Math.floor(Math.random() * 10);
+
+    if (randomInt % 2 === 0) {
+      // Success
+      setTimeout(() => resolve("done"), 2000);
+    } else {
+      // Failure
+      setTimeout(() => reject("error"), 2000);
+    }
+  });
+}
+
+async function getPromiseResponse() {
+  try {
+    const result = await fiftyFityChance();
+    console.log(`Yay! Promise resolved with response: ${result}`);
+  } catch (e) {
+    console.log(`Boo. Promise rejected with response: ${e}`);
+  }
+}
+
+getPromiseResponse();
