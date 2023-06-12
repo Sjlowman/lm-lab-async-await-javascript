1a63c2acfde5776c9de032a75bd1e9cfee1519e4
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-12 13:30:53 +0100

    Implement async for exercise 002

--- exercise002.js
+++ exercise002.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-
+/* Function to convert
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
 const fetchData = (apiEndPoint) => {
@@ -8,5 +8,20 @@ const fetchData = (apiEndPoint) => {
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
 };
+
+fetchData(jsonTypicode);
+*/
+
+const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+
+async function fetchData(apiEndPoint) {
+  try {
+    const response = await fetch(apiEndPoint);
+    const returnObj = await response.json();
+    console.log(returnObj);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData(jsonTypicode);
